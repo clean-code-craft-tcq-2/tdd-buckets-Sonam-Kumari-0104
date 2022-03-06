b0010cb86b102d97097ec872bedac837a0ba8e9b
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-06 18:01:57 +0530

    Create test-range.cpp

---
+++ test-range.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "typewise-alert.h"
+
+TEST_CASE("Check the range and total number of readings lying in each range") {
+}
