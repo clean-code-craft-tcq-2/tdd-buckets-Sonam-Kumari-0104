1dd93e40d560ae4632b87e4fb27615d6ba69e53c
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-07 20:49:05 +0530

    Update test-range.cpp

--- test-range.cpp
+++ test-range.cpp
@@ -2,8 +2,9 @@
 
 #include "test/catch.hpp"
 #include "rangeChecker.h"
+#include <string.h>
 
 TEST_CASE("Check the range and total number of readings lying in each range") {
   int chargingRange[] = {4,5};
-  REQUIRE(getRangeAndReadings(chargingRange)=="4-5, 2");         
+  REQUIRE(strcmp(getRangeAndReadings(chargingRange),"4-5, 2")==0);         
 }
