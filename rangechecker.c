f9f8958c4e61f5a3ba3032a59af1fefe60387a5a
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-07 21:17:03 +0530

    Update rangechecker.c

--- rangechecker.c
+++ rangechecker.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include "rangeChecker.h"
 
-const char* getRangeAndReadings(int* chargingRange){
-    return "4-5, 2";
+const char* getRangeAndReadings(int* chargingISamples){
+    size_t noOfSamples = numberOfSamples(chargingISamples);
+    int minValue = getMinValue(chargingISamples, noOfSamples);
+    int maxValue = getMaxValue(chargingISamples, noOfSamples);
+    char * chargingRangeAndReadings[15];
+    sprintf(chargingRangeAndReadings, "%d-%d, %d", minValue, maxValue, noOfSamples);
+    return "chargingRangeAndReadings";
   }
+
+size_t numberOfSamples(int* chargingISamples){
+    size_t noOfISamples = sizeof(chargingISamples)/sizeof(chargingISamples[0]);
+    return noOfISamples;
+}
+
+int getMinValue(int* Samples, int noOfSamples){
+    int min = Samples[0];
+    for(int i=1; i<noOfSamples; i++)
+    {
+        if (min > Samples[i]){
+            min = Samples[i];
+        }
+    }
+    return min;
+}
+
+int getMaxValue(int* Samples, int noOfSamples){
+    int max = Samples[0];
+    for(int i=1; i<noOfSamples; i++)
+    {
+        if (max < Samples[i]){
+            max = Samples[i];
+        }
+    }
+    return max;
+}
