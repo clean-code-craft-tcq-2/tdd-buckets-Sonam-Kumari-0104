1c4df77511f19c803322fab384b4c4642f69ee1b
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-07 21:26:34 +0530

    Update test-range.cpp

--- test-range.cpp
+++ test-range.cpp
@@ -6,5 +6,5 @@
 
 TEST_CASE("Check the range and total number of readings lying in each range") {
   int chargingRange[] = {4,5};
-  REQUIRE(strcmp(getRangeAndReadings(chargingRange),"4-5, 2")==0);         
+  REQUIRE(strcmp((getRangeAndReadings(chargingRange)),"4-5, 2"));         
 }
