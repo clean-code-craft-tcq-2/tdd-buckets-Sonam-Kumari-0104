6d4d82e256abe7c41fbc002a00a22e50ba95b685
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-07 21:18:33 +0530

    Update rangeChecker.h

--- rangeChecker.h
+++ rangeChecker.h
@@ -1,2 +1,4 @@
 const char* getRangeAndReadings(int* chargingISamples);
 size_t numberOfSamples(int* chargingISamples);
+int getMinValue(int* Samples, int noOfSamples);
+int getMaxValue(int* Samples, int noOfSamples);
