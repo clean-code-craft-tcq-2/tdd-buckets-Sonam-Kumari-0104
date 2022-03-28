577fd4d1c324a493b7a2f52841035e20d2ab2dc7
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-28 22:53:51 +0530

    Update test-range.cpp

--- test-range.cpp
+++ test-range.cpp
@@ -5,10 +5,10 @@
 #include <string.h>
 
 TEST_CASE("Check the range and total number of readings lying in each range") {
-  int arr[4] = {1, 2, 12, 11};
-  ReadingRange expectedReadingRange[2] = {{1, 2, 2}, {11, 12, 2}};
+  int arr[5] = {1, 3, 2, 12, 11};
+  ReadingRange expectedReadingRange[2] = {{1, 3, 3}, {11, 12, 2}};
   ReadingRangeList expectedReadingRangeList = {expectedReadingRange, 2};
-  ReadingRangeList actualReadingRangeList = getReadingRangeList(arr);
+  ReadingRangeList actualReadingRangeList = getReadingRangeList(arr, 5);
   ReadingRange *actualReadingRange = actualReadingRangeList.readingRanges;
   for (int i = 0; i < expectedReadingRangeList.totalReadingRanges; i++)
   {
