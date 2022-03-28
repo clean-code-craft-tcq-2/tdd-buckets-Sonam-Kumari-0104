e921bdadbf15e5c6c599432ae963c6a8d90e9979
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-03-28 22:30:47 +0530

    Update rangechecker.c

--- rangechecker.c
+++ rangechecker.c
@@ -22,7 +22,7 @@ int *sort(int *array, int size)
 ReadingRangeList getReadingRangeList(int *readingsArray)
 {
     size_t size = sizeof(readingsArray)/sizeof(readingsArray[0]);
-    int(*sortedArray) = sort(readingsArray, size);
+    int *sortedArray = sort(readingsArray, size);
     ReadingRange *readingRanges = (ReadingRange *)malloc(size * sizeof(ReadingRange));
     int start = sortedArray[0], end = sortedArray[0], count = 0, rangeIndex = 0;
     for (int i = 0; i < size; i++)
@@ -43,5 +43,9 @@ ReadingRangeList getReadingRangeList(int *readingsArray)
     }
     readingRanges[rangeIndex] = {start, end, count};
     ReadingRangeList readingRangeList = {readingRanges, rangeIndex + 1};
+    for (int i = 0; i < readingRangeList.totalReadingRanges; i++)
+    {
+        printf("%d-%d, %d\n", readingRanges[i].rangeStart, readingRanges[i].rangeEnd, readingRanges[i].numberOfReadings);
+    }
     return readingRangeList;
 }
